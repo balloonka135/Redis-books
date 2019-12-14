6b18f5cca10286b6914da082c5d6ee5372a52782
balloonka135 <balloonka135@gmail.com>
2019-12-14 18:10:22 +0100

    added queries

--- queries.py
+++ queries.py
@@ -9,17 +9,37 @@ def first_query():
     """
     tag = Tag.get(name='100-books')._pk
 
-    books_1 = Book.collection(tag1=tag).sort(by='average_rating').instances(lazy=True)
-    books_2 = Book.collection(tag2=tag).sort(by='average_rating').instances(lazy=True)
-    books_3 = Book.collection(tag3=tag).sort(by='average_rating').instances(lazy=True)
-    books_4 = Book.collection(tag4=tag).sort(by='average_rating').instances(lazy=True)
-
-    books = list(set(books_1) | set(books_2) | set(books_3) | set(books_4))
+    books = Book.collection(tag1=tag).sort(by='average_rating').instances(lazy=True)
 
     if len(books) >= 5:
         return books[-5:]
     else:
         return books
+
+
+def second_query():
+    """
+    2) Insert a book which author doesn't exists in the database
+    """
+
+    authors = Author.collection()
+    max_author = max([int(author) for author in authors]) + 1
+    books = Book.collection()
+    max_books = max([int(book) for book in books]) + 1
+
+    book = Book(sk_book_id=max_books, book_id=93892839, best_book_id=93892839, work_id=93892839, \
+                        books_count=1, isbn='211293892839', original_publication_year=2019, \
+                        original_title='The Guardians', title='The Guardians', language_code='eng', \
+                        average_rating=0, ratings_count=0, \
+                        work_ratings_count=0, \
+                        work_text_reviews_count=0, \
+                        author1=max_author, author2='None', author3='None', \
+                        tag1='None', tag2='None', tag3='None', tag4='None', tag5='None'
+                    )
+
+    author = Author(author_id=max_author, full_name='John Grisham')
+
+    return author
 
 
 def third_query():
@@ -35,6 +55,23 @@ def third_query():
     user = User.get(user_id=user_id)
 
     return user
+
+
+def fourth_query():
+    """
+    4) Best Book Recommendation by Similar Users
+    """
+    user = User.get(user_id = 20)
+    books_user = [r.hmget_dict('book_id') for r in Rating.collection(user_id = user, rating = 5).instances(lazy = True) if r.hmget_dict('book_id')['book_id'] != 'None']
+    users_similar = [r.hmget_dict('user_id') for u in books_user for r in Rating.collection(book_id = u['book_id'], rating = 5).instances(lazy = True)]
+    books_similar = [r.hmget_dict('book_id')['book_id'] for u in users_similar for r in Rating.collection(user_id = u['user_id'], rating = 5).instances(lazy = True) if r.hmget_dict('book_id')['book_id'] != 'None']
+    best_10_books = Counter(books_similar).most_common(10)
+
+    for b in best_10_books:
+        print(Book.get(pk=b[0]).hmget_dict('title'))
+
+    print('\n\n')
+
 
 
 def fifth_query():
@@ -55,6 +92,21 @@ def fifth_query():
         return books
 
 
+def sixth_query():
+    """
+    6) Most rated Books that were published in 2015 (Number of Rates)
+    """
+    books = Book.collection(original_publication_year='2015').instances()
+    ratings = []
+    ratings = [r.hmget_dict('book_id')['book_id'] for b in books for r in Rating.collection(book_id=b._pk).instances(lazy = True)]
+    num_rating_books = Counter(ratings).most_common(10)
+
+    for b in num_rating_books:
+        print(Book.get(pk=b[0]).hmget_dict('title'))
+
+    print('\n\n')
+
+
 def seventh_query():
     """
     update book 'Gone Girl'
@@ -67,6 +119,20 @@ def seventh_query():
         language_code='ru'
     )
     return book
+
+
+def eigth_query():
+    """
+    8) Delete the user the rated the least amount of books
+    """
+
+    user_ratings = Rating.collection().values_list('user_id', flat=True)
+    num_rating_users = Counter(user_ratings)
+    m = min(num_rating_users, key=num_rating_users.get)
+    [r.delete() for r in Rating.collection(user_id=m).instances()]
+    User.get(user_id=m).delete()
+
+    return User.get(user_id=m)
 
 
 def ninth_query():
@@ -94,17 +160,23 @@ def ninth_query():
     return result
 
 
+def tenth_query():
+    """
+        10) Users that rated Esteban's books with the highest rating per book.
+    """
+    author = Author.get(full_name = 'Esteban Zimnyi')
+    books = Book.collection(author1 = author)
+    ratings = [r.hmget_dict('user_id') for r in Rating.collection(book_id = books, rating = 5).instances()]
+    best_users = [u.hmget_dict('full_name') for u in User.collection(user_id = ratings).instances()]
+    print(best_users)
+
+
 if __name__ == '__main__':
-    start_time = time.time()
-    result = ninth_query()
-    print("--- %s seconds ---\n\n" % (time.time() - start_time))
+    result = fourth_query()
 
-    import itertools
-    new_dict = dict(itertools.islice(result.items(),15))
+    # for r in result:
+    #     print(r.hmget_dict('title'))
 
-    for key, value in new_dict.items():
-        print(key, value)
-
-    # print(result.hmget_dict('title', 'average_rating', 'original_publication_year', 'language_code'))
-
-
+    # start_time = time.time()
+    # result = first_query()
+    # print("--- %s seconds ---\n\n" % (time.time() - start_time))
